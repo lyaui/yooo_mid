39416efc340b09863c0c9a9ba50846be77cc83e2
Iris Huang <lyaui@outlook.com>
2024-10-16 20:58:53 +0800

    feat: add handleRoomChange event

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,48 +1,81 @@
-import type { RoomSpec } from "@/types";
+import { memo } from "react";
+import type { ChangeEvent } from "react";
+import type { RoomSpec, ResultRoom } from "@/types";
 import CustomInputNumber from "@/components/CustomInputNumber";
 
 type RoomProps = {
   roomSpec: RoomSpec;
+  allocatedRoom: ResultRoom;
+  onChange: (val: ResultRoom) => void;
 };
-function Room({ roomSpec }: RoomProps) {
-  const totalPeople = 5;
+function Room({ roomSpec, allocatedRoom, onChange }: RoomProps) {
+  const { adult = 0, child = 0 } = allocatedRoom || {};
+  const totalPeople = allocatedRoom?.adult + allocatedRoom?.child;
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const { roomPrice, adultPrice, childPrice } = roomSpec;
+    const updateValue = {
+      ...allocatedRoom,
+      [name]: +value,
+    };
+    updateValue.price =
+      roomPrice +
+      adultPrice * updateValue.adult +
+      childPrice * updateValue.child;
+    onChange(updateValue);
+  };
+
+  const guests = [
+    {
+      title: "大人",
+      desc: "年齡 20+",
+      props: {
+        min: 0,
+        max: 100,
+        step: 1,
+        name: "adult",
+        value: adult,
+        disabled: false,
+      },
+    },
+    {
+      title: "小孩",
+      desc: null,
+      props: {
+        min: 0,
+        max: 100,
+        step: 1,
+        name: "child",
+        value: child,
+        disabled: false,
+      },
+    },
+  ];
+
   return (
     <div className={"flex flex-col gap-3 py-6"}>
       <div className="mb-2 text-lg font-medium">
         房間： {totalPeople || 0} 人
       </div>
-      <div className="flex items-center justify-between">
-        <div>
-          <div>大人</div>
-          <span className="text-gray-400">年齡 20+</span>
+      {guests.map((_guest) => (
+        <div key={_guest.title} className="flex items-center justify-between">
+          <div>
+            <div>{_guest.title}</div>
+            {_guest.desc && (
+              <span className="text-gray-400">{_guest.desc}</span>
+            )}
+          </div>
+          <CustomInputNumber
+            {..._guest.props}
+            step={1}
+            onChange={handleChange}
+            onBlur={handleChange}
+          />
         </div>
-        <CustomInputNumber
-          min={0}
-          max={0}
-          step={1}
-          name={""}
-          value={0}
-          disabled={false}
-          onChange={() => {}}
-          onBlur={() => {}}
-        />
-      </div>
-
-      <div className="flex items-center justify-between">
-        <div>小孩</div>
-        <CustomInputNumber
-          min={0}
-          max={0}
-          step={1}
-          name={""}
-          value={0}
-          disabled={false}
-          onChange={() => {}}
-          onBlur={() => {}}
-        />
-      </div>
+      ))}
     </div>
   );
 }
 
-export default Room;
+export default memo(Room);
--- src/components/RoomAllocation.tsx
+++ src/components/RoomAllocation.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect, useMemo } from "react";
 import type { Guest, RoomSpec, ResultRoom } from "@/types";
 
+import getDefaultRoomAllocation from "@/utils/getDefaultRoomAllocation";
 import Room from "@/components/Room";
 
 type RoomAllocationProps = {
@@ -13,18 +15,45 @@ function RoomAllocation({
   roomsSpec = [],
   onChange,
 }: RoomAllocationProps) {
+  const [allocatedRooms, setAllocatedRooms] = useState<ResultRoom[]>([]);
+  const defaultRooms = useMemo(
+    () => getDefaultRoomAllocation(guest, roomsSpec),
+    [guest, roomsSpec],
+  );
+
+  const handleRoomChange = (idx: number) => (value: ResultRoom) => {
+    setAllocatedRooms((_preState) => {
+      const updateVal = [..._preState];
+      updateVal[idx] = value;
+      return updateVal;
+    });
+  };
+
+  useEffect(() => {
+    setAllocatedRooms(defaultRooms);
+  }, [defaultRooms]);
+
+  useEffect(() => {
+    onChange(allocatedRooms);
+  }, [allocatedRooms, onChange]);
+
   return (
     <div className="flex max-w-3xl flex-col gap-5 p-10">
       <h3 className="text-3xl font-extrabold">
-        住客人數：{guest.adult} 位大人，{guest.child}位小孩 / {roomsSpec.length}{" "}
-        房
+        住客人數：{guest.adult} 位大人，{guest.child} 位小孩 /{" "}
+        {roomsSpec.length} 房
       </h3>
       <div className="rounded-md border border-sky-200 bg-sky-100 p-5 text-gray-600">
-        尚未分配人數：{guest.adult} 位大人，{guest.child}位小孩
+        尚未分配人數：{guest.adult} 位大人，{guest.child} 位小孩
       </div>
       <div className="divide-y">
         {roomsSpec.map((_roomSpec, _idx) => (
-          <Room key={_idx} roomSpec={_roomSpec} />
+          <Room
+            key={_idx}
+            roomSpec={_roomSpec}
+            allocatedRoom={allocatedRooms[_idx]}
+            onChange={handleRoomChange(_idx)}
+          />
         ))}
       </div>
     </div>
