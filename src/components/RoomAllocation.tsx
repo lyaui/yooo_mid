24b5138f5d05213f628b7f93ffc6344095470901
Iris Huang <lyaui@outlook.com>
2024-10-16 16:15:39 +0800

    feat(RoomAllocation): render rooms array

--- src/components/RoomAllocation.tsx
+++ src/components/RoomAllocation.tsx
@@ -1,37 +1,28 @@
-import type { Guest, Room, ResultRoom } from "@/types";
+import type { Guest, Room as TRoom, ResultRoom } from "@/types";
 
-import CustomInputNumber from "@/components/CustomInputNumber";
+import Room from "@/components/Room";
 
 type RoomAllocationProps = {
   guest: Guest;
-  rooms: Room[];
+  rooms: TRoom[];
   onChange: (result: ResultRoom[]) => void;
 };
+
 function RoomAllocation({ guest, rooms = [], onChange }: RoomAllocationProps) {
-  const handleChange = (result: ResultRoom[]) => {
-    onChange(result);
-  };
   return (
     <div className="flex max-w-3xl flex-col gap-5 p-10">
       <h3 className="text-3xl font-extrabold">
         住客人數：{guest.adult} 位大人，{guest.children}位小孩 / {rooms.length}{" "}
         房
       </h3>
-
       <div className="rounded-md border border-sky-200 bg-sky-100 p-5 text-gray-600">
         尚未分配人數：{guest.adult} 位大人，{guest.children}位小孩
       </div>
-
-      <CustomInputNumber
-        min={0}
-        max={0}
-        step={1}
-        name={""}
-        value={0}
-        disabled={false}
-        onChange={() => {}}
-        onBlur={() => {}}
-      />
+      <div className="divide-y">
+        {rooms.map((_room, _idx) => (
+          <Room key={_idx} room={_room} />
+        ))}
+      </div>
     </div>
   );
 }
